26166d5dbaa1546115c5800d506e1b8c45b62bc5
muhammadhasansiddiqui <hasansiddiqui464@gamil.com>
2025-02-24 19:56:22 +0500

    work

--- src/app/components/AuthForm.jsx
+++ src/app/components/AuthForm.jsx
@@ -3,8 +3,8 @@
 import { useState } from "react";
 import GoogleLogo from "../../images/google-logo.png";
 import FbLogo from "../../images/facebook-icon.png";
-import { doc, setDoc } from "firebase/firestore"; 
-import { db } from "../../utils/firebase"; // Firestore ka import zaroori hai
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "../../utils/firebase";
 import AuthGuard from "../../utils/AuthGuard"
 import Phone from "../../images/Phone.png";
 import { auth, googleProvider } from "../../utils/firebase";
@@ -16,15 +16,15 @@ import {
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Image from "next/image";
-import { useRouter } from "next/navigation"; // ✅ Import useRouter
+import { useRouter } from "next/navigation";
 
 export default function AuthForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isLogin, setIsLogin] = useState(true);
-  const router = useRouter(); // ✅ Initialize useRouter
+  const router = useRouter();
 
- 
+
   const handleAuth = async () => {
     try {
       let result;
@@ -34,26 +34,26 @@ export default function AuthForm() {
       } else {
         result = await createUserWithEmailAndPassword(auth, email, password);
         toast.success("Signup Successful!");
-  
+
         // ✅ User ko Firestore me "users" collection me store karna
         const user = result.user;
         await setDoc(doc(db, "users", user.uid), {
           uid: user.uid,
           name: user.displayName || email.split("@")[0], // Agar naam na ho to email ka pehla hissa
           email: user.email,
-          photoURL: user.photoURL || "", 
+          photoURL: user.photoURL || "",
           createdAt: new Date(), // User ke signup ka time
         });
       }
-      
+
       console.log("User Info:", result.user);
       router.push("/ChatRoom");
-  
+
     } catch (error) {
       toast.error(error.message);
     }
   };
-  
+
 
   const handleGoogleLogin = async () => {
     try {
@@ -61,8 +61,8 @@ export default function AuthForm() {
       toast.success("Google Login Successful!");
       console.log("Google Login Success:", result.user);
 
-      router.push("/ChatRoom"); 
-      
+      router.push("/ChatRoom");
+
     } catch (error) {
       toast.error(error.message);
     }
@@ -78,79 +78,79 @@ export default function AuthForm() {
 
   return (
 
-<AuthGuard>
+    <AuthGuard>
 
-    <div className="flex flex-col w-full items-center justify-center min-h-screen bg-black text-white">
-      <div className="bg-[#111111] p-8 rounded-lg shadow-lg w-96">
-        <h2 className="text-2xl font-semibold text-center mb-4">
-          {isLogin ? "Log in to your account✨" : "Create an account ✨"}
-        </h2>
-        <p className="text-center text-gray-400 mb-6">
-          Welcome! Please enter your details.
-        </p>
+      <div className="flex flex-col w-full items-center justify-center min-h-screen bg-black text-white">
+        <div className="bg-[#111111] p-8 rounded-lg shadow-lg w-96">
+          <h2 className="text-2xl font-semibold text-center mb-4">
+            {isLogin ? "Log in to your account✨" : "Create an account ✨"}
+          </h2>
+          <p className="text-center text-gray-400 mb-6">
+            Welcome! Please enter your details.
+          </p>
 
-        <label className="block mb-2">
-          Email
-          <input
-            type="email"
-            placeholder="Enter your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="w-full p-2 mb-3 rounded bg-gray-800 text-white"
-          />
-        </label>
+          <label className="block mb-2">
+            Email
+            <input
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full p-2 mb-3 rounded bg-gray-800 text-white"
+            />
+          </label>
 
-        <label className="block mb-2">
-          Password
-          <input
-            type="password"
-            placeholder="Enter your password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full p-2 mb-4 rounded bg-gray-800 text-white"
-          />
-        </label>
+          <label className="block mb-2">
+            Password
+            <input
+              type="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full p-2 mb-4 rounded bg-gray-800 text-white"
+            />
+          </label>
 
-        <button
-          onClick={handleAuth}
-          className="w-full p-3 rounded bg-gradient-to-r from-[#ff416c] to-[#ff4b2b] text-white font-semibold cursor-pointer mb-4 hover:from-[#ff4b2b] hover:to-[#ff416c]"
-        >
-          {isLogin ? "Login" : "Sign Up"}
-        </button>
+          <button
+            onClick={handleAuth}
+            className="w-full p-3 rounded bg-gradient-to-r from-[#ff416c] to-[#ff4b2b] text-white font-semibold cursor-pointer mb-4 hover:from-[#ff4b2b] hover:to-[#ff416c]"
+          >
+            {isLogin ? "Login" : "Sign Up"}
+          </button>
 
-        <button
-          onClick={handleGoogleLogin}
-          className="w-full mt-3 bg-black border border-gray-600 p-2 rounded flex items-center justify-center transition-all duration-300 hover:bg-gradient-to-r hover:from-[#ff416c] hover:to-[#ff4b2b]"
-        >
-          <Image src={GoogleLogo} alt="Google Logo" width={25} height={25} className="mr-2" />
-          Sign in with Google
-        </button>
+          <button
+            onClick={handleGoogleLogin}
+            className="w-full mt-3 bg-black border border-gray-600 p-2 rounded flex items-center justify-center transition-all duration-300 hover:bg-gradient-to-r hover:from-[#ff416c] hover:to-[#ff4b2b]"
+          >
+            <Image src={GoogleLogo} alt="Google Logo" width={25} height={25} className="mr-2" />
+            Sign in with Google
+          </button>
 
-        <button
-          onClick={handleFbLogin}
-          className="w-full mt-3 bg-black border border-gray-600 p-2 rounded flex items-center justify-center transition-all duration-300 hover:bg-gradient-to-r hover:from-[#ff416c] hover:to-[#ff4b2b]"
-        >
-          <Image src={FbLogo} alt="FB Logo" width={25} height={25} className="mr-2" />
-          Sign in with Facebook
-        </button>
+          <button
+            onClick={handleFbLogin}
+            className="w-full mt-3 bg-black border border-gray-600 p-2 rounded flex items-center justify-center transition-all duration-300 hover:bg-gradient-to-r hover:from-[#ff416c] hover:to-[#ff4b2b]"
+          >
+            <Image src={FbLogo} alt="FB Logo" width={25} height={25} className="mr-2" />
+            Sign in with Facebook
+          </button>
 
-        <button
-          onClick={handlePhoneLogin}
-          className="w-full mt-3 bg-black border border-gray-600 p-2 rounded flex items-center justify-center transition-all duration-300 hover:bg-gradient-to-r hover:from-[#ff416c] hover:to-[#ff4b2b]"
-        >
-          <Image src={Phone} alt="Phone Logo" width={25} height={25} className="mr-2" />
-          Sign in with SMS
-        </button>
+          <button
+            onClick={handlePhoneLogin}
+            className="w-full mt-3 bg-black border border-gray-600 p-2 rounded flex items-center justify-center transition-all duration-300 hover:bg-gradient-to-r hover:from-[#ff416c] hover:to-[#ff4b2b]"
+          >
+            <Image src={Phone} alt="Phone Logo" width={25} height={25} className="mr-2" />
+            Sign in with SMS
+          </button>
 
-        <p
-          onClick={() => setIsLogin(!isLogin)}
-          className="mt-4 text-center text-gray-400 cursor-pointer hover:underline"
-        >
-          {isLogin ? "Create an account" : "Already have an account? Log in"}
-        </p>
+          <p
+            onClick={() => setIsLogin(!isLogin)}
+            className="mt-4 text-center text-gray-400 cursor-pointer hover:underline"
+          >
+            {isLogin ? "Create an account" : "Already have an account? Log in"}
+          </p>
+        </div>
+        <ToastContainer position="top-right" autoClose={3000} hideProgressBar />
       </div>
-      <ToastContainer position="top-right" autoClose={3000} hideProgressBar />
-    </div>
     </AuthGuard>
   );
 }
